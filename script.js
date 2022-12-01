5d4e20c0f3e4208eb40e6d36a1819da6df4288e9
mnabiei1379 <mnabiei1379@gmail.com>
2022-12-01 15:02:40 +1100

    4th Commit

--- script.js
+++ script.js
@@ -14,56 +14,7 @@ function writePassword() {
 }
 
 function generatePassword() {
-    // This loop check whether the password criteria length is valid or not
-    // its then stored in var passwordLength
-    var passwordLength = window.prompt("Enter your password criteria:\nlength of the password (must be between 8-128 characters)");
-    while (passwordLength < 8 || passwordLength > 128) {
-        passwordLength = window.prompt("Please re-enter the password length (must be between 8-128 characters)");
-    }
-    // This will validate the length of the password
-    window.alert("You chose password length to be = "+passwordLength);
-
-    // Now we wnat to check the character types of the password, we convert 
-    // the var all to uppercase incase user puts in y or n, For every prompt
-    // the character type chosen is validated
-
-    // The character types are stored in a struct
-    var characterTypes = {
-        lowerCase: "abcdefghijklmnopqrstuvwxyz",
-        upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-        numeric: "0123456789",
-        special: [" ","!",`"`,"#","$","%","&","'","()","*","+",",","-",".","/",":",";","<","=",">","?","@","[","]","^","_","{","|","}","~","]"],
-
-    }    
-
-    var characterTypeLowercase = window.prompt("Enter your character type:\n1) Lowercase\nPlease select Y or N");
-    characterTypeLowercase = characterTypeLowercase.toUpperCase();
-    if (characterTypeLowercase === "Y") {
-        window.alert("Lowercase type validated!");
-
-        
-    }
-
-    var characterTypeUppercase = window.prompt("Enter your character type:\n2) Uppercase\nPlease select Y or N");
-    characterTypeUppercase = characterTypeUppercase.toUpperCase();
-    if (characterTypeUppercase === "Y") {
-        window.alert("Uppercase type validated!");
-
-    }
-
-    var characterTypeNumeric = window.prompt("Enter your character type:\n3) Numeric\nPlease select Y or N");
-    characterTypeNumeric = characterTypeNumeric.toUpperCase();
-    if (characterTypeNumeric === "Y") {
-        window.alert("Numeric type validated!");
-    }
-
-    var characterTypeSpecial = window.prompt("Enter your character type:\n4) Special Characters\nPlease select Y or N");
-    characterTypeSpecial = characterTypeSpecial.toUpperCase();
-    if (characterTypeSpecial === "Y") {
-        window.alert("Special characters validated!");
-    }
-
-
+    passCriteria();
     // Password generator code
     // Used code from: 
     // This code showed an example of a password generator in javascript
@@ -113,7 +64,56 @@ function generatePassword() {
     }
 }
 
+function passCriteria() {
+ // This loop check whether the password criteria length is valid or not
+    // its then stored in var passwordLength
+    var passwordLength = window.prompt("Enter your password criteria:\nlength of the password (must be between 8-128 characters)");
+    while (passwordLength < 8 || passwordLength > 128) {
+        passwordLength = window.prompt("Please re-enter the password length (must be between 8-128 characters)");
+    }
+    // This will validate the length of the password
+    window.alert("You chose password length to be = "+passwordLength);
 
+    // Now we wnat to check the character types of the password, we convert 
+    // the var all to uppercase incase user puts in y or n, For every prompt
+    // the character type chosen is validated
+
+    // The character types are stored in a struct
+    var characterTypes = {
+        lowerCase: "abcdefghijklmnopqrstuvwxyz",
+        upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+        numeric: "0123456789",
+        special: [" ","!",`"`,"#","$","%","&","'","()","*","+",",","-",".","/",":",";","<","=",">","?","@","[","]","^","_","{","|","}","~","]"],
+
+    }    
+
+    var characterTypeLowercase = window.prompt("Enter your character type:\n1) Lowercase\nPlease select Y or N");
+    characterTypeLowercase = characterTypeLowercase.toUpperCase();
+    if (characterTypeLowercase === "Y") {
+        window.alert("Lowercase type validated!");
+
+        
+    }
+
+    var characterTypeUppercase = window.prompt("Enter your character type:\n2) Uppercase\nPlease select Y or N");
+    characterTypeUppercase = characterTypeUppercase.toUpperCase();
+    if (characterTypeUppercase === "Y") {
+        window.alert("Uppercase type validated!");
+
+    }
+
+    var characterTypeNumeric = window.prompt("Enter your character type:\n3) Numeric\nPlease select Y or N");
+    characterTypeNumeric = characterTypeNumeric.toUpperCase();
+    if (characterTypeNumeric === "Y") {
+        window.alert("Numeric type validated!");
+    }
+
+    var characterTypeSpecial = window.prompt("Enter your character type:\n4) Special Characters\nPlease select Y or N");
+    characterTypeSpecial = characterTypeSpecial.toUpperCase();
+    if (characterTypeSpecial === "Y") {
+        window.alert("Special characters validated!");
+    }
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
