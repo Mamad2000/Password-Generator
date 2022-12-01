83c334558bba82e36290df48ce0ab16f3ffc2743
mnabiei1379 <mnabiei1379@gmail.com>
2022-12-01 22:58:44 +1100

    11th commit

--- script.js
+++ script.js
@@ -6,9 +6,12 @@ var lowerCasedCharacters = ['a','b','c','d','e','f','g','h','i','j','k','l','m',
 
 var upperCasedCharacters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 
-var specialCharacters = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.'];
-
-
+var specialCharacters = ['@','%','+','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.'];
+var characterTypeLowercase;
+var characterTypeUppercase;
+var characterTypeNumeric;
+var characterTypeSpecial;
+var passwordLength;
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -30,65 +33,60 @@ function generatePassword() {
     // URL:
     // https://dev.to/code_mystery/random-password-generator-using-javascript-6a#:~:text=querySelector(%22%23password%22)%3B,%2C%20()%20%3D%3E%20%7B%20passwordInput.
     var password01 = document.querySelector("#password");
-    var password01 = "PASSWORD";
+    var password01 = "";
+    var validCharacterSet = [];
+
+    console.log("Character type lowercase =  " + characterTypeLowercase);
+    console.log("Character type Uppercase =  " + characterTypeUppercase);
+    console.log("Character type Numeric =  " + characterTypeNumeric);
+    console.log("Character type Special =  " + characterTypeSpecial);
+
     // If user chose lowercase type
     if (characterTypeLowercase === "Y") {
-        window.alert("Test")
-        for (var i = 0; i <= passwordLength; i++) {
-            window.alert(i);
-            var randomNumber = Math.floor(Math.random() * lowerCasedCharacters.length);
-            password01 += chars.substring(randomNumber, randomNumber +1);
-
-        }
+        validCharacterSet = validCharacterSet.concat(lowerCasedCharacters); 
+        console.log("Valid Character set =  " + validCharacterSet);
     }
-    // if user chose Uppercase type
-    else if (characterTypeUppercase === "Y") {
-        for (var j = 0; j <= passwordLength; j++) {
-            var randomNumber = Math.floor(Math.random() * upperCasedCharacters.length);
-            password01 += chars.substring(randomNumber, randomNumber +1);
+    if (characterTypeUppercase === "Y") {
+        validCharacterSet = validCharacterSet.concat(upperCasedCharacters); 
+        console.log("Valid Character set =  " + validCharacterSet);
 
-        }
     }
-    // if user chose numeric type
-    else if (characterTypeNumeric === "Y") {
-        for (var k = 0; k <= passwordLength; k++) {
-            var randomNumber = Math.floor(Math.random() * numericCharacters.length);
-            password01 += chars.substring(randomNumber, randomNumber +1);
+    if (characterTypeNumeric === "Y") {
+        validCharacterSet = validCharacterSet.concat(numericCharacters);
+        console.log("Valid Character set =  " + validCharacterSet);
 
-        }
     }
-    // if user chose special type
-    else if (characterTypeSpecial === "Y") {
-        for (var l = 0; l <= passwordLength; l++) {
-            var randomNumber = Math.floor(Math.random() * specialCharacters.length);
-            password01 += chars.substring(randomNumber, randomNumber +1);
+    if (characterTypeSpecial === "Y") {
+        validCharacterSet = validCharacterSet.concat(specialCharacters);
+        console.log("Valid Character set =  " + validCharacterSet);
 
-        }
-    }
-    // If all characters were chosen
-    else if (characterTypeLowercase === "Y" && characterTypeUppercase === "Y" && characterTypeSpecial === "Y" && characterTypeNumeric === "Y") {
-        for (var m = 0; m <= passwordLength; m++) {
-            var randomNumber = Math.floor(Math.random() * specialCharacters.length + numericCharacters.length +upperCasedCharacters.length +lowerCasedCharacters.length);
-            password01 += chars.substring(randomNumber, randomNumber +1);
+    } 
+    if (characterTypeLowercase === "N" && characterTypeUppercase === "N" && characterTypeNumeric === "N" && characterTypeSpecial === "N") {
+        window.alert("At least one type must be selected!");
+        password01 = "No Password generated because no character Type was selected!";
+        console.log("Valid Character set =  " + validCharacterSet);
 
-        }
+        return;
     }
 
-    document.querySelector("#password").value = password01;
-    window.alert(password01);
+    for (var i = 0; i <= passwordLength; i++) {
+        var randomNumber = Math.floor(Math.random() * validCharacterSet.length);
+        // password01 += chars.substring(randomNumber, randomNumber +1);
+
+        password01 += validCharacterSet[randomNumber];
+    }
+
+    // document.querySelector("#password").value = password01;
+    // window.alert("The Password is:  " + password01);
     // writePassword();
-    // Ask user if they want to generate a new password
-    var generateAgain = window.confirm("Generate new password?");
+    return password01;
 
-    if(generateAgain) {
-        generatePassword();
-    }
 }
 
 function passCriteria() {
  // This loop check whether the password criteria length is valid or not
     // its then stored in var passwordLength
-    var passwordLength = window.prompt("Enter your password criteria:\nlength of the password (must be between 8-128 characters)");
+    passwordLength = window.prompt("Enter your password criteria:\nlength of the password (must be between 8-128 characters)");
     // If user pressed Cancel, immediately end function
     if (!passwordLength) {
         return;
@@ -101,18 +99,9 @@ function passCriteria() {
 
     // Now we wnat to check the character types of the password, we convert 
     // the var all to uppercase incase user puts in y or n, For every prompt
-    // the character type chosen is validated
+    // the character type chosen is validated   
 
-    // The character types are stored in a struct
-    // var characterTypes = {
-    //     lowerCase: "abcdefghijklmnopqrstuvwxyz",
-    //     upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-    //     numeric: "0123456789",
-          
-
-    // }    
-
-    var characterTypeLowercase = window.prompt("Enter your character type:\n1) Lowercase\nPlease select Y or N");
+    characterTypeLowercase = window.prompt("Enter your character type:\n1) Lowercase\nPlease select Y or N");
     characterTypeLowercase = characterTypeLowercase.toUpperCase();
     if (characterTypeLowercase === "Y") {
         window.alert("Lowercase type validated!");        
@@ -120,7 +109,7 @@ function passCriteria() {
         window.alert("Lowercase type not chosen!");
     }
 
-    var characterTypeUppercase = window.prompt("Enter your character type:\n2) Uppercase\nPlease select Y or N");
+    characterTypeUppercase = window.prompt("Enter your character type:\n2) Uppercase\nPlease select Y or N");
     characterTypeUppercase = characterTypeUppercase.toUpperCase();
     if (characterTypeUppercase === "Y") {
         window.alert("Uppercase type validated!");
@@ -130,7 +119,7 @@ function passCriteria() {
     }
 
 
-    var characterTypeNumeric = window.prompt("Enter your character type:\n3) Numeric\nPlease select Y or N");
+    characterTypeNumeric = window.prompt("Enter your character type:\n3) Numeric\nPlease select Y or N");
     characterTypeNumeric = characterTypeNumeric.toUpperCase();
     if (characterTypeNumeric === "Y") {
         window.alert("Numeric type validated!");
@@ -139,7 +128,7 @@ function passCriteria() {
     }
 
 
-    var characterTypeSpecial = window.prompt("Enter your character type:\n4) Special Characters\nPlease select Y or N");
+    characterTypeSpecial = window.prompt("Enter your character type:\n4) Special Characters\nPlease select Y or N");
     characterTypeSpecial = characterTypeSpecial.toUpperCase();
     if (characterTypeSpecial === "Y") {
         window.alert("Special characters validated!");
